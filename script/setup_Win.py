2d47f59eb38251bbef75b5429290a39156118b86
Bianca Sosnovski <bsosnovski@aol.com>
2019-09-29 21:10:31 -0400

    Update setup_Win.py

--- script/setup_Win.py
+++ script/setup_Win.py
@@ -1,16 +1,33 @@
-## To compile WeBWorK_Classlist script into a stand alone program in a Windows machine
-## Software: Python 3.7, py2exe, py2win
-## In a Command window do the following:
-## cd to the directory of the script WeBWorK_Classlist
-## python "setup_Win.py" py2exe
-##
-from distutils.core import setup
-import py2exe, sys, os
+## Run the build process by running the command 'python setup.py build'
+#
+# If everything works well you should find a subdirectory in the build
+# subdirectory that contains the files needed to run the application
+#
+# To create a stand alone app:
+# Go to terminal
+# Type: cd /Users/Beebee/Desktop/wwclasslist
+# Then type: python setup_Win2.py build
 
-sys.argv.append('py2exe')
+application_title = "CUNYFirst2WW_Converter" #what you want to application to be called
+main_python_file = "WWClasslist.py" #the name of the python file you use to run the program
 
-setup(data_files=[],
-    options = {'py2exe': {'bundle_files': 3, 'compressed': True}},
-    console = [{'script': "WWClasslist.py"}],
-    zipfile = "CUNYFirst2WW_converter.zip",
-)
+import sys
+import cx_Freeze
+from cx_Freeze import setup, Executable
+
+base = None
+if sys.platform == "win32":
+    base = "Win32GUI"
+
+includes = ["atexit","re", "os"]
+
+
+setup(
+        name = application_title,
+        version = "2.0",
+        description = "Script to convert CUNY Portal Rosters to a WeBWorK classlist file ",
+        author="Bianca Sosnovski, QCC",
+        options = {"build_exe" : {"includes" : includes }},
+        executables = [Executable(main_python_file, base = base)])
+
+
